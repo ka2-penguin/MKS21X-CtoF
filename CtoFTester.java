50f8c109d75c45a497ab020e3fc4c19be34f3c3a
Maya Mori <morikids314@gmail.com>
2021-09-23 15:58:39 -0400

    first commit

---
+++ CtoFTester.java
@@ -0,0 +1,9 @@
+// the functuions should have one parameter
+// temp which should be double type
+// The function should return a double, the temperature
+
+public class CtoFTester{
+    public static void main(String[] args){
+        System.out.println("Hi!");
+    }
+}
